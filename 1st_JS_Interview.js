296bcaf017a32775c529952f977efb0eb2c6b4c6
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-18 16:40:29 +0530

    JS_Interview_Practice

---
+++ 1st_JS_Interview.js
@@ -0,0 +1,45 @@
+// let nums =  [1,2,[2,[1],3,],3,4,5]
+// 2.console.log(!!0);
+// 3.console.log(!!'');
+// 4.console.log(!!NaN);
+// 5.console.log(!!false);
+// 6.console.log(!!undefine);
+// let x = 3;
+// let y = "3";
+// 7.console.log(x + y);
+// 8. console.log(x - y);
+// 9. console.log(x == y);
+// 10. console.log(x === y);
+// var x = 220;
+// var y = "Hello";
+// var z = undefined;
+// 11.console.log(x || y )
+// 12.console.log(x || z )
+// 13.console.log(x && z )
+// 14.console.log(x && z )
+// 15.find rajesh is present or not in string
+// 16.let str = "my is Rajesh saini" 
+// 17.make a 1D array and convert into 2D array
+// 18.what is function declaration and definition with example
+// 19.define arrow function , Anonymous, HOF function
+// 20.what is data type and tell name all data type 
+// 21. undefine and null kis type ke datatype hai 
+// 22.what is different b/t undefine and undeclared
+// 23.what is spread operator and how to use it 
+// 24.what is destructuring 
+// 25.what is this keys explain with example 
+// function object  (){
+//     console.log(this)
+// }
+// function object  (){
+//     console.log(this)
+// }
+// 26.what is setTimeout and setInterval with example 
+// 27.what is different b/t setTimeout and SetInterval
+// 28.what will be output 
+function Interval (){
+    console.log((2),1000)
+    console.log(1)
+    console.log((3),20000)
+    console.log(4)
+}
