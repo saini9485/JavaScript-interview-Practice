06ee6163a5276dd86c468204a2f11addf5ab8ef2
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-26 08:39:57 +0530

    Currying

--- 21Aug_CARS24.js
+++ 21Aug_CARS24.js
@@ -39,13 +39,12 @@ question:- 4
 // console.log(add(5)(2)(4)(6)()) 
 
 answer :-4
-// function add (a){
-//     return function (b){
-//         if(b)return add (a+b)
-//     }
-// }
-// console.log(add(5)(2)(4)(5)()) //undefine
-
+function add (a){
+    return function (b){
+        if(b)return add (a+b) 
+    }
+}
+console.log(add(5)(2)(4)(5)()) //undefine 
 //why it is coming undefine
 
 
@@ -54,20 +53,20 @@ question:-5
 // const result = calc.add(10).multiply(5).subtract(30).add(10);
 // console.log(result.total)
 solution :-5
-const calc = {
-    total:0,
-    add(a){
-        this.total +=a;
-        return this ;
-    },
-    multiply (a){
-        this.total *=a;
-        return this;
-    },
-    subtract (a){
-        this.total -=a;
-        return this;
-    },
-};
-const result = calc.add(10).multiply(5).subtract(30).add(10);
-console.log(result.total)
+// const calc = {
+//     total:0,
+//     add(a){
+//         this.total +=a;
+//         return this ;
+//     },
+//     multiply (a){
+//         this.total *=a;
+//         return this;
+//     },
+//     subtract (a){
+//         this.total -=a;
+//         return this;
+//     },
+// };
+// const result = calc.add(10).multiply(5).subtract(30).add(10);
+// console.log(result.total)
---
+++ 26Aug_Currying_Que.js
@@ -0,0 +1,59 @@
+question: -1; /* */
+// function add (a){
+//     return function (b){
+//         if(b)return add (a+b)
+//     }
+// }
+// console.log(add(5)(2)(4)(5)()) //undefine
+
+question: -2; /*Convert sum(2,6,1) to sum(2)(6)(1)*/
+// function sum(a) {
+//   return function (b) {
+//     return function (c) {
+//       return a + b + c;
+//     };
+//   };
+// }
+// console.log(sum(2)(6)(1)); //9
+
+question: -3; /* */
+// function sum(operation) {
+//     return (a) => {
+//         return (b) => {
+//         if(operation === "sum")
+//                   return a + b;
+//                     else if(operation === "multiply")
+//                     return a * b;
+//                     else if(operation === "divide")
+//                     return a / b;
+//                     else if(operation === "subtract")
+//                     return a - b;
+//                     else return "No / Invalid Operation Selected"
+//         }
+//     }
+// }
+
+question: -4; /*Write a currying function that takes infinite arguments. */
+// function infinite(a) {
+//   return function (b) {
+//     return function (c) {
+//       return function (d) {
+//         // ...
+//         // ...
+//         // ...
+//         return a+b+c+d+...+n;
+//       };
+//     };
+//   };
+// }
+
+question :-5 /* *///recursive solution
+const sum = function(a) {
+    return function(b) {
+        if (b) {
+            return sum(a + b);
+        } else {
+            return a;
+        }
+    }
+}
