c45aafdd48d60ee778bf48538502375204bb3ee7
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-11-07 01:28:52 +0530

    Interview Questions

--- JS/Debouncing_Throatling/Debouncing.js
+++ JS/Debouncing_Throatling/Debouncing.js
@@ -1,24 +1,40 @@
-// //Without Debouncing..
-// let counter = 0;
-// function getData (){
-//     console.log("FetchData"+counter++)
-// }
-// getData() 
-
-//with Debouncing ......
+//Without Debouncing..
 let counter = 0;
 function getData (){
     console.log("FetchData"+counter++)
 }
-// getData()
-
-function Debouncing (callB,delay){
-let  timer;
-return function (...args){
-    if(timer)clearTimeout(timer)
-   timer= setTimeout(()=>{
-        callB();
-    },delay)
+//  getData() 
+function Debouncing(call, wait){
+let timer;
+return function (){
+    if(timer)clearTimeout(timer) // clear 
+    timer =setTimeout(()=>{
+        call()
+    },wait)
 }
 }
 const BetterFunction = Debouncing(getData,2000)
+
+
+
+
+
+
+
+//with Debouncing ......
+// let counter = 0;
+// function getData (){
+//     console.log("FetchData"+counter++)
+// }
+// // getData()
+
+// function Debouncing (callB,delay){
+// let  timer;
+// return function (...args){
+//     if(timer)clearTimeout(timer)
+//    timer= setTimeout(()=>{
+//         callB();
+//     },delay)
+// }
+// }
+// const BetterFunction = Debouncing(getData,2000)
