331b93f7a086b26f95ac4916e70e87fc70575416
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-11-13 12:13:11 +0530

    interView Question

--- JS/OutPutBase_Questions/PlacementPre.js
+++ JS/OutPutBase_Questions/PlacementPre.js
@@ -1,1 +1,43 @@
-console.log("hi")
+/*Placement Pre. With Vasanth Sir*/
+Question:-1 //OR Gate 
+// console.log(10 | 2)  //Output 10
+// console.log(2 | 10)  //10
+// console.log(1 | 2)  // 3
+// console.log(4 | 3)  // 7
+//doubts this is question
+
+Question:-2 // AND Gate
+// console.log(10 & 2) // output 2   
+// console.log(2 & 10) // output 2   
+/* binary => 8 4 2 1
+        10=>  1 0 1 0  
+        2=>  0 0 1 0
+             */
+
+Question:-3 //XOR Gate
+// console.log(10^2)  // 8
+// console.log(10^3)  // 8
+/*
+ */
+
+Question:-4 //
+// console.log(2<<1) //4 =>move one from left 
+// console.log(3<<2) // 12 => number * 2^ exponent
+// console.log(10<<0) // 10 
+
+/*
+* 8 4 2 1
+*2-> 0 0 1 0 
+3->0 0 1 1  
+*/
+
+Question:-5  // 
+console.log(2>>1) //1 =>move one from Right 
+console.log(3>>2) // 0 => number * 2^ exponent
+console.log(10>>0) // 10 number/ 2^exponent
+/*
+* 8 4 2 1
+*2-> 0 0 1 0 
+3->0 0 1 1  
+*/
+
