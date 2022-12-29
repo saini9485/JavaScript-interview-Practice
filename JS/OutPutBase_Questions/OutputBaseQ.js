4e4c14381a9043ff50553090b28f6a5137ee0b45
Rajesh Saini <saini222302@gmail.com>
2022-12-29 23:33:25 +0530

    interview

--- JS/OutPutBase_Questions/OutputBaseQ.js
+++ JS/OutPutBase_Questions/OutputBaseQ.js
@@ -183,6 +183,16 @@ var x = 23;
   })();
 })();
 
+QUESTION:-19
+let a=[1,2,3,4]
+let b=[5,6,7,8];
+let newArr = []
+
+for(let i=0;i<a.length;i++){
+    newArr.push(a[i])
+    newArr.push(b[i])
+}
+console.log(newArr) //[1,5,2,6,3,7,4,8];
 
 
 
