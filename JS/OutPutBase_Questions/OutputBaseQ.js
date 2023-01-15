dbae8b8b17aa028eda8b004c8b92982fcdfedf41
Rajesh Saini <saini222302@gmail.com>
2023-01-16 01:27:24 +0530

    JS interview Questiuon

--- JS/OutPutBase_Questions/OutputBaseQ.js
+++ JS/OutPutBase_Questions/OutputBaseQ.js
@@ -195,4 +195,23 @@ for(let i=0;i<a.length;i++){
 console.log(newArr) //[1,5,2,6,3,7,4,8];
 
 
+console.log(null); //null;
+console.log(+null); //0;
+console.log(undefined); //undefined;
+console.log(+undefined); //NaN
+console.log(["a"] + ["b"] + ["c"]); // "ab"
+console.log([] + []); // ""
+console.log(![] + []); // "false", because ![] returns false.
+console.log(+false); //0
+console.log(NaN); // NaN
+console.log(+NaN); // NaN
+console.log(+""); // 0
+console.log(+null === 0); //true
+console.log(!!1 === true); //true
+console.log(Math.max()); //-Infinity
+console.log(Math.min()); //Infinity
+console.log(NaN == NaN); //false
+console.log(typeof NaN.toString() === "string"); //true;
+
+
 
