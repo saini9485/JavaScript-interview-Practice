66f536207289a4a1988bd13ecc9b8c3b496af17d
Rajesh Saini <saini222302@gmail.com>
2023-01-31 09:08:08 +0530

    interview Questions

--- 28janInterview.js
+++ 28janInterview.js
@@ -28,15 +28,40 @@
 //how we can change in 1 to 5 without using let 
 
 for (var i = 0; i < 5; i++) {
-    function a (i){
-       setTimeout(() => {
-        console.log(i)
-    }, 1000)  
-    i++
+    function a(i) {
+        setTimeout(() => {
+            console.log(i)
+        }, 1000)
+        i++
     }
-  a(i)
-} 
+    a(i)
+}
 
 //1. convert "aircampus" into capital later using array method 
 // 2. polyfills 
+
+question: -1 //what will output this question 
+
+let x = true
+let count = 0;
+setTimeout(() => {
+    x = false
+    console.log("false x", x)
+}, 1000)
+
+while (1) {
+    console.log("x", x)
+    if (x) {
+        count++
+        console.log("count", count)
+    }
+
+}
+ //x true
+// count 1
+// x true
+// count 2......
+
+question:-2 //
+
 
