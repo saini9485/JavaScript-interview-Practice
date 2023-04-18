ef5fb02c2498693cdbac40efdb8357d73d94612e
Rajesh Saini <saini222302@gmail.com>
2023-04-18 16:25:42 +0530

    interview

--- JS/2nd_JS_Interview.js
+++ JS/2nd_JS_Interview.js
@@ -4,7 +4,7 @@
 //for true = 1 , for false = 0
 // console.log(1<2<3); //1<2=>true , 1<3 => true
 //  console.log(3>2>1); //3>2 => false , 0>1 = > false 
-//  console.log(0.1+0.2==0.3) // .1+.2 => 30000000004 , .3 = > 3 false
+  console.log(0.1+0.2==0.3) // .1+.2 => 30000000004 , .3 = > 3 false
 console.log(1<2<4<1)//1<2=> true , 1<4 => true , 1<1 => false
 console.log(1>2>4>0)//1>2 => false , 0>4 => false , 0>0=> false
 console.log(3>2>0>1)// false 
