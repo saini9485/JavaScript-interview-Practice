912e85ac555525b68c7da23eee8e9cfc36df5a85
Rajesh Saini <saini222302@gmail.com>
2023-05-07 16:04:23 +0530

    interview

---
+++ JS/OutPutBase_Questions/RealInterview.jsx
@@ -0,0 +1,15 @@
+Question: -1; //what will be output (ResourcesPlane.io Interview )
+
+let arr = [1, 2, 3, 4, 5, 6];
+for (i = 0; i < arr.length; i++) {
+  setTimeout(function () {
+    console.log(arr[i]);
+  }, 3000);
+}
+
+//undefined 6 times
+
+Questions:-2 //what is different in between 
+this.setState({name:"xyz"})
+and
+this.setState.name = "xyz"
